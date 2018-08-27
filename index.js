fe7d097a6b591dafb3eca82d817833dcd7f5fd4b
william <willshinji@gmail.com>
2018-08-27 12:34:59 -0300

    resize image

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const sharp = require('sharp');
+
+sharp('./assets/images/node_logo.jpeg')
+  .resize(300)
+  .toFile('./assets/images/node_logo_resized.jpeg', (err, info) => {
+    if(err) throw err;
+    console.log(info);
+  });
