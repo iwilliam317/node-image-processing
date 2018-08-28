ae1dfab3652b4fbcf7c89756319b10b5d75381aa
william <willshinji@gmail.com>
2018-08-28 00:01:24 -0300

    rotate image

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const sharp = require('sharp');
 
-// RESIZE
+// Rotate - Resize
 sharp('./assets/images/node_logo.jpeg')
+  .rotate(180)
   .resize(300)
   .toFile('./assets/images/node_logo_resized.jpeg', (err, info) => {
     if(err) throw err;
     console.log(info);
   });
+
