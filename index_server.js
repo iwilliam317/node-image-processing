f03b506270422fcc81935745c6fa3fcb62373ca8
william <willshinji@gmail.com>
2018-08-29 00:21:28 -0300

    uploads image*

--- index_server.js
+++ index_server.js
@@ -1,2 +1,11 @@
 const app = require('express')();
 const multer = require('multer');
+
+const upload = multer({ dest: './assets/images/' });
+
+app.post('/resize/:height/:width', upload.single('image'), (request, response, next) => {
+  console.log(request.file);
+  response.send(request.file)
+});
+
+app.listen(3000);
