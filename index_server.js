2b39ab037221dbdd0a782c501270c4ac1f7e84d6
william <willshinji@gmail.com>
2018-08-30 00:14:18 -0300

    save resized image

--- index_server.js
+++ index_server.js
@@ -1,14 +1,22 @@
 const app = require('express')();
 const multer = require('multer');
 const sharp = require('sharp');
+const fs = require('fs');
 
 const upload = multer({ dest: './assets/images/' });
 
 app.post('/resize/:height/:width', upload.single('image'), (request, response, next) => {
   response.set('content-type', 'image/jpg');
+
+  let image = `assets/images/${request.file.filename}_${request.params.height}_${request.params.width}.jpg`;
   sharp(`./assets/images/${request.file.filename}`)
     .resize(Number(request.params.height), Number(request.params.width))
-    .pipe(response)
+    .toFile(image, error => {
+      if (error) throw error;
+
+      fs.createReadStream(image).pipe(response)
+    })
+
 });
 
 app.listen(3000);
