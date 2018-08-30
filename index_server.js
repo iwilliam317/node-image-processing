c28358809f446ae58a2386576c9369828564c50c
william <willshinji@gmail.com>
2018-08-30 00:50:04 -0300

    add nodemon

--- index_server.js
+++ index_server.js
@@ -9,13 +9,24 @@ app.post('/resize/:height/:width', upload.single('image'), (request, response, n
   response.set('content-type', 'image/jpg');
 
   let image = `assets/images/${request.file.filename}_${request.params.height}_${request.params.width}.jpg`;
-  sharp(`./assets/images/${request.file.filename}`)
-    .resize(Number(request.params.height), Number(request.params.width))
-    .toFile(image, error => {
-      if (error) throw error;
 
+  fs.stat(image, (error, stat) => {
+    if (error) throw error;
+
+    if(stat.isFile()){
       fs.createReadStream(image).pipe(response)
-    })
+    }
+    else{
+      sharp(`./assets/images/${request.file.filename}`)
+        .resize(Number(request.params.height), Number(request.params.width))
+        .toFile(image, error => {
+          if (error) throw error;
+
+          fs.createReadStream(image).pipe(response)
+        })      
+    }
+  });
+
 
 });
 
